f2f19c4f7899635494c446a80a6e0b75ae330b74
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-06-08 08:25:20 -0700

    need to finish/ figure out how to test code

---
+++ 07 Directory Creation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Script Name:               Directory Creation
+# Author:                    Gerald
+# Date of latest revision:   6/7/2023
+# Purpose:                   User gives files and python takes you to file path       
+# Instructions:     
+#Script must ask the user for a file path and read a user input string into a variable.
+# This will use the input() command. Next line will prompt you to type in something that used as s a variable
+#Script must use the os.walk() function from the os library.
+# import os --> os.walk(input)
+#Script must enclose the os.walk() function within a python function that hands it the user input file path.
+
+
+#Declartion of varibles:
+path = input("Type in a file path")
+
+
+#Declaration of functions:
+def lookup ():
+    import os
+    os.walk(input)
+
+
+
+
+#Main
