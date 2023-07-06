31bfbfc24fbec6a0a0bf90a7952920e4835118d8
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-07-06 19:27:39 -0400

    Update 11 Psutil.py

--- 11 Psutil.py
+++ 11 Psutil.py
@@ -18,16 +18,37 @@
 #Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
 
 #Declartion of varibles:
-
+cpu_times = psutil.cpu_times()
+user_time = cpu_times.user
+kernel = cpu_times.system
+idle = cpu_times.idle
+priority = cpu_times.nice
+io = cpu_times.iowait
+irq = cpu_times.irq
+softirq = cpu_times.softirq
+steal = cpu_times.steal
+guest = cpu_times.guest
 
 #Declaration of functions:
 
-
-
-
 #Main
 
+import psutil
 
-import Psutil
-print(psutil.cpu_times())
-print()
+print("Time in seconds spent by normal processes executing in user mode:", cpu_times)
+
+print("Time in seconds spent by processes executing in kernel mode:", kernel)
+
+print("Time in seconds when system was idle:", idle)
+
+print("Time spent by priority processes executing in user mode:", priority)
+
+print("Time spent waiting for I/O to complete:", io)
+
+print("Time spent for servicing hardware interrupts:", irq)
+
+print("Time spent for servicing software interrupts:", softirq)
+
+print("Time spent by other operating systems running in a virtualized environment:", steal)
+
+print("Time spent running a virtual CPU for guest operating systems:", guest)
